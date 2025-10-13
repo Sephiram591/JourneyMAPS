426577a09ca8a387c8f28a48d3efb42084e8bcb3
sephiram591 <helaman.flores591@gmail.com>
2025-10-13 13:14:29 -0400

    Update docs conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,26 +3,19 @@ import sys
 from datetime import datetime
 
 # Add project root to sys.path so Sphinx can find the package
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 project = "jmaps"
 author = "Helaman Flores"
 # copyright = f"{datetime.now().year}, {author}"
 
 extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
-    # "sphinx.ext.napoleon",
-    # "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
-    "myst_parser",
+    "sphinx.ext.intersphinx",
 ]
-
-autosummary_generate = True
-autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    "show-inheritance": True,
-}
 
 templates_path = ["_templates"]
 exclude_patterns = [
@@ -31,13 +24,18 @@ exclude_patterns = [
     ".DS_Store",
 ]
 
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
-napoleon_google_docstring = True
-napoleon_numpy_docstring = True
 
-master_doc = "index"
+# master_doc = "index"
 
 autodoc_mock_imports = [
     "pmag",

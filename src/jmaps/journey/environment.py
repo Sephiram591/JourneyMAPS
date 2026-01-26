5b0ca571e88d77e54f61cf6fdac831e6343a9cc4
sephiram591 <helaman.flores591@gmail.com>
2026-01-26 07:33:44 -0500

    Added tqdm req to read the docs

--- src/jmaps/journey/environment.py
+++ src/jmaps/journey/environment.py
@@ -1,12 +1,11 @@
 """Environment containers for named parameter sets.
 
-`JEnv` wraps a mapping of parameter names to `JParam` with helpers for
-filtering by semantic type and extracting hashable values used for caching.
+`JEnv` wraps a mapping of parameter names to parameters with helpers for
+evaluating lambda functions and extracting hashable values used for caching.
 """
-from jmaps.journey.param import JParam, ParamType
-
-class JEnv(dict[str, JParam]):
-    def __init__(self, name, params: dict[str, JParam]|None=None):
+from typing import Any
+class JEnv(dict[str, Any]):
+    def __init__(self, name, params: dict[str, Any]|None=None, triggered_lambdas:dict[str, Any]|None=None, invisible_params:set[str]|None=None):
         """JEnv is a class that represents an environment used in a journey.
         It wraps a dictionary that is enforced to contain only JParams and a name for the environment.
         
@@ -17,19 +16,35 @@ class JEnv(dict[str, JParam]):
         super().__init__()
         self.name = name
         self.update(params)
-    def get_params(self, types: list[ParamType]=[ParamType.SET, ParamType.VAR, ParamType.OPT, ParamType.LAMBDA]):
-        """Returns a dictionary of all JParams matching the given types."""
-        return {name: param for name, param in self.items() if param.type in types}
-    def get_values(self, types: list[ParamType]=[ParamType.SET, ParamType.VAR, ParamType.OPT, ParamType.LAMBDA]):
-        """Returns a dictionary of all JParam.value objects matching the given types."""
-        return {name: param.value for name, param in self.items() if param.type in types}
-    def get_hashable_values(self):
-        """Returns a dictionary of all JParam.value objects that affect the results of the journey. Used for saving and loading Path results."""
-        return self.get_values(types=[ParamType.SET, ParamType.VAR, ParamType.LAMBDA])
-    def get_types(self, types: list[ParamType]=[ParamType.SET, ParamType.VAR, ParamType.OPT, ParamType.LAMBDA]):
-        """Returns a dictionary of all JParam.type objects."""
-        return {name: type(param) for name, param in self.get_values(types=types).items()}
-    def reset_lambdas(self):
-        """Resets the values of all lambda parameters in the environment."""
-        for param in self.get_params(types=[ParamType.LAMBDA]):
-            param.reset_value()
+        self.triggered_lambdas = triggered_lambdas if triggered_lambdas is not None else {}
+        self.invisible_params = invisible_params if invisible_params is not None else []
+    def get_values(self):
+        """Returns a dictionary of all values, evaluating any triggered lambda functions"""
+        values = {}
+        for name, param in self.items():
+            if name in self.triggered_lambdas:
+                param = self.evaluate_lambda(name, param)
+            values[name] = param
+        return values
+    def get_visible_values(self):
+        """Returns a dictionary of all visible parameters. Used for saving and loading Path results."""
+        values = {}
+        for name, param in self.items():
+            if name not in self.invisible_params:
+                if name in self.triggered_lambdas:
+                    param = self.evaluate_lambda(name, param)
+                values[name] = param
+        return values
+    def evaluate_lambda(self, lambda_id:str, lambda_val):
+        if self.triggered_lambdas[lambda_id] is None:
+            self.triggered_lambdas[lambda_id] = lambda_val()
+        return self.triggered_lambdas[lambda_id]
+    def reset_lambda_triggers(self):
+        """Resets the values of all lambda triggers in the environment."""
+        for key in self.triggered_lambdas.keys():
+            self.triggered_lambdas[key] = None
+    def update(self, other):
+        if isinstance(other, JEnv):
+            self.triggered_lambdas.update(other.triggered_lambdas)
+            self.invisible_params.update(other.invisible_params)
+        super().update(other)
